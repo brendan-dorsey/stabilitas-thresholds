890735afb85abb2370c87fbb90a671a828c024a5
Brendan Dorsey <beeg.dorsey@gmail.com>
2017-06-30 10:27:09 -0700

    Filter loads and processes data

--- src/stabilitasfilter.py
+++ src/stabilitasfilter.py
@@ -8,16 +8,16 @@ def main():
 class StabilitasFilter(object):
     def __init__(self, cities_filename, cleaned=True):
         """
-        Instantiate a StabilitasFilter object with cities specified in the
-        passed file.
+        Instantiate a StabilitasFilter object with cities specified in
+        the passed file.
         """
         self._load_cities(cities_filename, cleaned)
         self.window_to_minutes_converter = {
-                                    "12h": 720,
-                                    "1d": 1440,
-                                    "1w": 10080,
-                                    "4w": 40320
-                                }
+            "12h": 720,
+            "1d": 1440,
+            "1w": 10080,
+            "4w": 40320
+        }
 
     def _load_cities(
             self,
@@ -81,12 +81,13 @@ class StabilitasFilter(object):
                                 self.cities_df["longitude"]
                             )
 
-    def fit(self,
-            filename,
-            resample_size,
-            window_size,
-            anomaly_threshold
-        ):
+    def fit(
+        self,
+        data_filename,
+        resample_size,
+        window_size,
+        anomaly_threshold
+    ):
         """
         Fit model to reports included in file. This method loads the data,
         preprocesses it, builds time series, and identifies anomalies.
@@ -97,29 +98,87 @@ class StabilitasFilter(object):
             window_size - str, length of window for rolling mean and
                 standard deviation
                 Valid inputs: "12h", "1d", "1w", "4w"
+            anomaly_threshold - int, number of standard deviations above
+                the mean a resample bucket needs to be in order to pass
+                through the filter to the next layer
         Output: fit model ready to return anomaly information
         """
-        pass
+        self._load_data(data_filename)
 
-    def _load_data(self, filename):
+    def _load_data(self, data_filename):
         """
         Load data from the given file.
 
         Input: filename
         Output: Self with stored and processed report data
         """
-        pass
+        column_names = [
+            "lat",
+            "lon",
+            "id",
+            "source_id",
+            "account_id",
+            "title",
+            "created_on",
+            "updated_on",
+            "start_ts",
+            "until_ts",
+            "report_type",
+            "notes",
+            "layer_id",
+            "severity"
+        ]
 
-    def _preprocess_data(self, DataFrame):
+
+
+        self.reports_df = pd.read_table(
+            data_filename,
+            header=None,
+            names=column_names
+        )
+        self._preprocess_data()
+
+
+    def _preprocess_data(self):
         """
         Clean input data and build engineered features for use by the
         model.
 
-        Input: Raw dataframe
-        Output: Cleaned and engineered dataframe.
+        Input: self with raw dataframe initialized
+        Output: self with cleaned and engineered dataframe
         """
-        pass
+        target_columns = [
+            "lat",
+            "lon",
+            "id",
+            "title",
+            "start_ts",
+            "report_type",
+            "severity"
+        ]
+        self.reports_df.dropna(axis=0, how="any", inplace=True)
+        self.reports_df = self.reports_df[target_columns]
+        self.reports_df["start_ts"] = pd.to_datetime(
+                                        self.reports_df["start_ts"],
+                                        unit="s",
+                                        errors="ignore"
+                                    )
 
+        def severity_score_quadratic(severity_rating):
+            if severity_rating == "low":
+                return 1
+            elif severity_rating == "moderate":
+                return 4
+            elif severity_rating == "medium":
+                return 9
+            elif severity_rating == "high":
+                return 16
+            elif severity_rating == "extreme":
+                return 25
+            else:
+                return 4
+        self.reports_df["severity_quadratic"] =\
+            self.reports_df["severity"].map(severity_score_quadratic)
 
     def _build_cities(self):
         pass
@@ -129,6 +188,9 @@ class StabilitasFilter(object):
 
     def test(self):
         print "Cities Loaded, shape: ", self.cities_df.shape
+        print "Reports Loaded, shape: ", self.reports_df.shape
+        print ""
+        print self.reports_df.info()
         print "Skeleton working"
 
 if __name__ == '__main__':
--- src/thresholds.py
+++ src/thresholds.py
@@ -1,9 +1,17 @@
 from stabilitasfilter import StabilitasFilter
 
 def main():
-    # Filepath assumes running this script from stabilitas-thresholds/ dir
+    # Filepaths assume running this script from stabilitas-thresholds/ dir
     cities_filename = "data/cities300000.csv"
     filter_layer = StabilitasFilter(cities_filename, cleaned=True)
+
+    data_filename = "data/reports_12DEC16-26DEC16.tsv"
+    filter_layer.fit(
+        data_filename,
+        resample_size=3,
+        window_size="1w",
+        anomaly_threshold=1
+    )
 
 
     filter_layer.test()
