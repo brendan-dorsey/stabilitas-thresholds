940438f00f00b2ed1125a73cdfc917dbaba91173
Brendan Dorsey <beeg.dorsey@gmail.com>
2017-07-03 02:01:25 -0300

    Completed First Model. Generated ROC Curve.

--- src/test_finder.py
+++ src/test_finder.py
@@ -1,8 +1,10 @@
 from stabilitasfinder import StabilitasFinder
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import time
 from datetime import datetime
+from sklearn.metrics import auc
 plt.style.use("ggplot")
 
 
@@ -21,7 +23,38 @@ def main():
 
     finder.fit()
     finder.predict_proba()
-    print finder.predict()
+    # print finder.predict()
+
+    thresholds = np.linspace(0.00, 1, 100)
+    false_positive_rates = []
+    true_positive_rates = []
+    for threshold in thresholds:
+        conf_mat = finder.predict(threshold=threshold)
+        fpr = float(conf_mat[0][1]) / (conf_mat[0][1] + (conf_mat[0][0]) + 1)
+        tpr = float(conf_mat[1][1]) / (conf_mat[1][1] + (conf_mat[1][0]) + 1)
+
+        false_positive_rates.append(fpr)
+        true_positive_rates.append(tpr)
+
+    area = auc(false_positive_rates, true_positive_rates)
+    fig, ax = plt.subplots(1, figsize=(8,8))
+
+    ax.plot(
+        false_positive_rates,
+        true_positive_rates,
+        label="ROC curve (area = {:0.2f})".format(area)
+    )
+    ax.plot([0,1], [0, 1], linestyle="--", color="k")
+    ax.scatter(0.2, 0.99, color="k", label="goal")
+
+    ax.set_xlabel("False Positive Rate")
+    ax.set_ylabel("True Positive Rate")
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_title("ROC Curve for First Model")
+    plt.legend(loc="lower right")
+    plt.show()
+
 
 
 
